9603913cc15b653c0fed293bf35660bb1b10cec1
20PI <20pimk@gmail.com>
2024-03-31 14:29:11 +0200

    Added upload feature

--- src/main/java/twenty/pye/serverzipper/ServerZipper.java
+++ src/main/java/twenty/pye/serverzipper/ServerZipper.java
@@ -7,69 +7,135 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.UUID;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 public final class ServerZipper extends JavaPlugin implements CommandExecutor {
-
+    private static final String FILEBIN_URL = "https://filebin.net";
+    private static final String UPLOAD_ENDPOINT = "/%s/%s";
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (label.equalsIgnoreCase("zip")) {
-            if (args.length < 1) {
-                sender.sendMessage(ChatColor.RED + "You must provide a file path!");
+        switch (label.toLowerCase()) {
+            case "upload":
+            case "zip":
+                if (args.length < 1) {
+                    sender.sendMessage(ChatColor.RED + "You must provide a file path!");
+                    return true;
+                }
+
+                File worldContainer = getServer().getWorldContainer();
+                String path = worldContainer.getPath() + "/" + args[0];
+                File fileToProcess = new File(path);
+
+                if (!fileToProcess.exists()) {
+                    sender.sendMessage(ChatColor.RED + "The file provided does not exist!");
+                    return true;
+                }
+
+                if (label.equalsIgnoreCase("zip")) {
+                    sender.sendMessage(ChatColor.YELLOW + "Attempting to zip provided file...");
+                    zipFileAsync(fileToProcess, fileToProcess.getParentFile().getPath() + "/" + fileToProcess.getName() + ".zip", sender);
+                } else {
+                    sender.sendMessage(ChatColor.YELLOW + "Attempting to upload file...");
+                    uploadFileAsync("serverzipper", "serverzipper" + generateRandomString(6), fileToProcess.getName(), fileToProcess, sender);
+                }
+                break;
+
+            case "zipserver":
+                sender.sendMessage(ChatColor.YELLOW + "Attempting to zip server...");
+                zipFileAsync(getServer().getWorldContainer(), ".server.zip", sender);
+                break;
+
+            default:
                 return false;
-            }
-
-            File worldContainer = getServer().getWorldContainer();
-            String path = worldContainer.getPath() + "/" + args[0];
-            File toZip = new File(path);
-
-            if (!toZip.exists()) {
-                sender.sendMessage(ChatColor.RED + "The file provided does not exist!");
-                return false;
-            }
-
-            sender.sendMessage(ChatColor.YELLOW + "Attempting to zip provided file...");
-            zipAsync(toZip, toZip.getParentFile().getPath() + "/" + toZip.getName() + ".zip", sender);
-        } else if (label.equalsIgnoreCase("zipserver")) {
-            sender.sendMessage(ChatColor.YELLOW + "Attempting to zip server...");
-            zipAsync(getServer().getWorldContainer(),   ".server.zip", sender);
         }
 
-        return false;
+        return true;
     }
 
-    public void zipAsync(File fileOrDirectoryToZip, String outputPath, CommandSender sender) {
+    private void uploadFileAsync(String cid, String bin, String filename, File file, CommandSender sender) {
+        getServer().getScheduler().runTaskAsynchronously(this, () -> uploadFile(cid, bin, filename, file, sender));
+    }
+
+    private void uploadFile(String cid, String bin, String filename, File file, CommandSender sender) {
+        HttpURLConnection connection = null;
+
+        try {
+            URL url = new URL(String.format(FILEBIN_URL + UPLOAD_ENDPOINT, bin, filename));
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("POST");
+            connection.setDoOutput(true);
+
+            String boundary = Long.toHexString(System.currentTimeMillis());
+            connection.setRequestProperty("Content-Type", "multipart/form-data; boundary=" + boundary);
+
+            try (DataOutputStream outputStream = new DataOutputStream(connection.getOutputStream())) {
+                outputStream.writeBytes("--" + boundary + "\r\n");
+                outputStream.writeBytes("Content-Disposition: form-data; name=\"cid\"\r\n\r\n");
+                outputStream.writeBytes(cid + "\r\n");
+                outputStream.writeBytes("--" + boundary + "\r\n");
+                outputStream.writeBytes("Content-Disposition: form-data; name=\"file\"; filename=\"" + filename + "\"\r\n\r\n");
+
+                try (FileInputStream fileInputStream = new FileInputStream(file)) {
+                    byte[] buffer = new byte[4096];
+                    int bytesRead;
+                    while ((bytesRead = fileInputStream.read(buffer)) != -1) {
+                        outputStream.write(buffer, 0, bytesRead);
+                    }
+                }
+
+                outputStream.writeBytes("\r\n--" + boundary + "--\r\n");
+                outputStream.flush();
+            }
+
+            int responseCode = connection.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_CREATED) {
+                sender.sendMessage(ChatColor.GREEN + "Upload successful: " + String.format("https://filebin.net/%s/%s", bin, filename));
+            } else {
+                sender.sendMessage(ChatColor.RED + "File upload failed. Response code: " + responseCode);
+                Bukkit.getLogger().warning("File upload failed. Response code: " + responseCode);
+            }
+        } catch (IOException e) {
+            Bukkit.getLogger().severe("An error occurred while uploading the file: " + e.getMessage());
+        } finally {
+            if (connection != null) {
+                connection.disconnect();
+            }
+        }
+    }
+
+    private static String generateRandomString(int length) {
+        String uuid = UUID.randomUUID().toString().replace("-", "");
+        return uuid.substring(0, length);
+    }
+
+    private void zipFileAsync(File fileOrDirectoryToZip, String outputPath, CommandSender sender) {
         getServer().getScheduler().runTaskAsynchronously(this, () -> {
             try {
-                zip(fileOrDirectoryToZip, outputPath, sender);
-            } catch (Exception e) {
+                zipFile(fileOrDirectoryToZip, outputPath);
+                sender.sendMessage(ChatColor.GREEN + "Successfully zipped file!");
+            } catch (IOException e) {
                 sender.sendMessage(ChatColor.RED + "Failed to zip file!");
-                e.printStackTrace();
+                Bukkit.getLogger().severe("An error occurred while zipping the file: " + e.getMessage());
             }
         });
     }
 
-    public void zip(File fileOrDirectoryToZip, String outputPath, CommandSender sender) {
-        try {
-            FileOutputStream fos = new FileOutputStream(outputPath);
-            ZipOutputStream zos = new ZipOutputStream(fos);
+    private void zipFile(File fileOrDirectoryToZip, String outputPath) throws IOException {
+        try (
+                FileOutputStream fos = new FileOutputStream(outputPath);
+                ZipOutputStream zos = new ZipOutputStream(fos)
+        ) {
             if (fileOrDirectoryToZip.isDirectory()) {
                 zipDirectory(fileOrDirectoryToZip, fileOrDirectoryToZip.getName(), zos);
             } else {
                 zipFile(fileOrDirectoryToZip, fileOrDirectoryToZip.getName(), zos);
             }
-            zos.close();
-            fos.close();
-            sender.sendMessage(ChatColor.GREEN + "Successfully zipped file!");
-        } catch (IOException e) {
-            sender.sendMessage(ChatColor.RED + "Failed to zip file!");
-            e.printStackTrace();
         }
     }
 
@@ -91,8 +157,7 @@ public final class ServerZipper extends JavaPlugin implements CommandExecutor {
             return;
         }
 
-        try {
-            FileInputStream fis = new FileInputStream(fileToZip);
+        try (FileInputStream fis = new FileInputStream(fileToZip)) {
             ZipEntry zipEntry = new ZipEntry(fileName);
             zos.putNextEntry(zipEntry);
             byte[] buffer = new byte[1024];
@@ -100,10 +165,8 @@ public final class ServerZipper extends JavaPlugin implements CommandExecutor {
             while ((length = fis.read(buffer)) >= 0) {
                 zos.write(buffer, 0, length);
             }
-            fis.close();
         } catch (IOException e) {
             Bukkit.getLogger().warning("Failed to zip file " + fileName + ": " + e.getMessage());
-            Bukkit.getLogger().info("Proceeding...");
         }
     }
 
@@ -111,12 +174,6 @@ public final class ServerZipper extends JavaPlugin implements CommandExecutor {
     public void onEnable() {
         getCommand("zip").setExecutor(this);
         getCommand("zipserver").setExecutor(this);
-
+        getCommand("upload").setExecutor(this);
     }
-
-    @Override
-    public void onDisable() {
-
-    }
-
 }
